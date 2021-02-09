dcf169c27cc52394e3514e667e6c08942941faa4
WilsonWann <37257533+WilsonWann@users.noreply.github.com>
2021-02-09 10:26:39 +0800

    webpack OK

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import HelloWorld from './HelloWorld';
+import HelloWorld from './HelloWorld';
 import _ from 'lodash';
 
-const a = [true, null]
-const b = a.filte(i => i === true)
-
 render(
-    // <HelloWorld />,
+    <HelloWorld />,
     document.getElementById('root')
 );
--- src/index2.js
+++
@@ -1,7 +0,0 @@
-import { render } from 'react-dom';
-import _ from 'lodash';
-
-
-render(
-    document.getElementById('root')
-);
